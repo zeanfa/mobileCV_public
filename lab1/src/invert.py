6fdef57ee423169c2ca2884c502b0bf4c5487a0c
Aleksei Denisov <zeanfa@gmail.com>
2021-09-13 19:59:32 +0300

    lab1 fix

--- lab1/src/invert.py
+++ lab1/src/invert.py
@@ -1,31 +1,70 @@
-import numpy as np
+# ITMO University
+# Mobile Computer Vision course
+# 2020
+# by Aleksei Denisov
+# denisov@itmo.ru
+
 import cv2
+import numpy as np
 
-print('Press 4 to Quit the Application\n')
+# gstreamer_pipeline returns a GStreamer pipeline for capturing from the CSI camera
+# Defaults to 1280x720 @ 60fps
+# Flip the image by setting the flip_method (most common values: 0 and 2)
+# display_width and display_height determine the size of the window on the screen
 
-#Open Default Camera
-cap = cv2.VideoCapture(0)
 
-while(cap.isOpened()):
-    #Take each Frame
-    ret, frame = cap.read()
-    
-    #Flip Video vertically (180 Degrees)
-    frame = cv2.flip(frame, 180)
+def gstreamer_pipeline(
+    capture_width=1280,
+    capture_height=720,
+    display_width=1280,
+    display_height=720,
+    framerate=30,
+    flip_method=0,
+):
+    return (
+        "nvarguscamerasrc ! "
+        "video/x-raw(memory:NVMM), "
+        "width=(int)%d, height=(int)%d, "
+        "format=(string)NV12, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink max-buffers=1 drop=true"
+        % (
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
 
-    invert = ~frame
 
-    # Show video
-    cv2.imshow('Cam', frame)
-    cv2.imshow('Inverted', invert)
+def show_camera():
+    # To flip the image, modify the flip_method parameter (0 and 2 are the most common)
+    print(gstreamer_pipeline(flip_method=4))
+    cap = cv2.VideoCapture(gstreamer_pipeline(flip_method=4), cv2.CAP_GSTREAMER)
+    if cap.isOpened():
+        window_handle = cv2.namedWindow("CSI Camera", cv2.WINDOW_AUTOSIZE)
+        # Window
+        while cv2.getWindowProperty("CSI Camera", 0) >= 0:
+            ret_val, frame = cap.read()
 
-    # Exit if "4" is pressed
-    k = cv2.waitKey(1) & 0xFF
-    if k == 52 : #ord 4
-        #Quit
-        print ('Good Bye!')
-        break
+            invert = ~frame
 
-#Release the Cap and Video   
-cap.release()
-cv2.destroyAllWindows()
+            # Show video
+            cv2.imshow('Inverted', invert)
+            # This also acts as
+            keyCode = cv2.waitKey(1) & 0xFF
+            # Stop the program on the ESC key
+            if keyCode == 27:
+                break
+        cap.release()
+        cv2.destroyAllWindows()
+    else:
+        print("Unable to open camera")
+
+
+if __name__ == "__main__":
+    show_camera()
